e568c405f1d2f7f3c4eca898399ef081fb381fb7
Lola <Lola@DESKTOP-1NL18RV>
2022-04-02 22:32:11 +0200

    Add project files.

---
+++ Glavna.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsDnevnik
+{
+    public partial class Glavna : Form
+    {
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void osobeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Osoba frm_Osoba = new Osoba();
+            frm_Osoba.Show();
+        }
+    }
+}
